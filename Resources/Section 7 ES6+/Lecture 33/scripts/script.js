dab048b15bb82c9761211da037c6216dc8b86a41
Matt Conforti <mattconforti21@gmail.com>
2021-02-15 16:25:14 -0500

    added documentation and JSON destructuring

--- Resources/Section 7 ES6+/Lecture 33/scripts/script.js
+++ Resources/Section 7 ES6+/Lecture 33/scripts/script.js
@@ -1,6 +1,14 @@
 fetch('https://api.nasa.gov/planetary/apod?api_key=tBnm0Up2BS8xHOsgIILAi5c1o4YjcaQmSMBTIePX')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    .then(response => {
+        if (response.status === 200) {
+            return response.json();
+        }
+    })
+    .then(data => {
+        console.log(data);
+        const {date, explanation, media_type, service_version, title, url} = data;
+        console.log(`Date: ${date}`);
+    })
     // catch any errors - 400 & 500 level client/server http status codes
     // (rejected promises)
     .catch(error => console.log(error.message));
