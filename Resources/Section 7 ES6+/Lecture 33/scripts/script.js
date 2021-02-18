8527e02ca69118f93478e08ca36bbb4da658bb59
Matt Conforti <mattconforti21@gmail.com>
2021-02-18 16:30:46 -0500

    add search functionality & now using correct API root/endpoint

--- Resources/Section 7 ES6+/Lecture 33/scripts/script.js
+++ Resources/Section 7 ES6+/Lecture 33/scripts/script.js
@@ -1,14 +1,20 @@
-fetch('https://api.nasa.gov/planetary/apod?api_key=tBnm0Up2BS8xHOsgIILAi5c1o4YjcaQmSMBTIePX')
-    .then(response => {
-        if (response.status === 200) {
-            return response.json();
-        }
-    })
-    .then(data => {
-        console.log(data);
-        const {date, explanation, media_type, service_version, title, url} = data;
-        console.log(`Date: ${date}`);
-    })
-    // catch any errors - 400 & 500 level client/server http status codes
-    // (rejected promises)
-    .catch(error => console.log(error.message));
+document.getElementById('search_b').addEventListener('click', (e) => {
+    // get value of search box to query API
+    const searchTerm = document.getElementById('in1').value;
+    // if the search term exists (is not ""), use fetch API to search
+    if (searchTerm) {
+        fetch(`https://images-api.nasa.gov/search?q=${searchTerm}`)
+            .then(response => {
+                if (response.status === 200) {
+                    return response.json();
+                }
+            })
+            .then(data => {
+                console.log(data);
+                // destructure data & assign to elements on our page
+            })
+            // catch any errors - 400 & 500 level client/server http status codes
+            // (rejected promises)
+            .catch(error => console.log(error.message));
+    }
+});
